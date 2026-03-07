fec6cd205e18af13d261c654b22be1abdf77c296
minseo110416 <minseo1104@g.skku.edu>
2026-03-08 01:12:53 +0900

    Delete puts.c

--- puts.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-
-  printf("Hi, this statement is using printf\n");
-  puts("Hi, I'm using puts instead of printf"); 	// Check that puts has no \n.
-
-  return 0;
-}
